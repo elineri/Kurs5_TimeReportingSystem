8232fc7c57404e16170ba62ad48928133d047bd5
elineri <elin.ericstam@gmail.com>
2022-03-29 13:44:40 +0200

    Created classes for database

---
+++ TimeReportingSystem.Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeReportingSystem.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+        [Required]
+        public string ProjectName { get; set; }
+    }
+}
---
+++ TimeReportingSystem.Models/TimeReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TimeReportingSystem.Models
+{
+    public class TimeReport
+    {
+        [Key]
+        public int TimeReportId { get; set; }
+        public int? EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public int? ProjectId { get; set; }
+        public Project Project { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int WorkedHours { get; set; }
+        public string Note { get; set; }
+    }
+}
