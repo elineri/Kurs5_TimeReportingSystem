8f29c1f85c3f7563db5fdfa07bdca33056280ea6
elineri <elin.ericstam@gmail.com>
2022-03-29 13:31:03 +0200

    Add project files.

---
+++ TimeReportingSystem.Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeReportingSystem.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
