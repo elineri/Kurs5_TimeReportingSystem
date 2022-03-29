922a45990fc6b65beb613f4260dd84de1a64b490
elineri <elin.ericstam@gmail.com>
2022-03-29 13:50:30 +0200

    Added new project for API. Added new Model folder with DbContext class

---
+++ TimeReportingSystem.API/Model/TimeReportDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeReportingSystem.Models;
+
+namespace TimeReportingSystem.API.Model
+{
+    public class TimeReportDbContext : DbContext
+    {
+        public TimeReportDbContext(DbContextOptions<TimeReportDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeReport> TimeReports { get; set; }
+    }
+}
